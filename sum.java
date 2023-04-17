672616d008fbbd610e1e6d4c95c1addf2ef7db5b
zewyQ <zwey7948@gmail.com>
2023-04-17 23:24:49 +0200

    sum

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+ 
+class Main {
+    public static void main(String[] args) {
+   
+        double a = Math.random();
+        double b = Math.random();
+        System.out.println(Double.sum(a, b));
+
+
+
+
+
+
+    }
+    
+    }
+
+
